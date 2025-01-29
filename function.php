06bb34d8bf81cfd0c0787207be3a62825a43e340
myDesktop <developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.com>
2025-01-29 19:23:06 +0600

    User define function

---
+++ function.php
@@ -0,0 +1,25 @@
+<?php
+    /* function myName(){
+        echo 'Forhad Hossain';
+    }
+    myName(); */
+
+    /* function age($age){
+        echo $age;
+    }
+    age(2); */
+
+    /* function info($name, $age){
+        echo 'My name is '.$name.'</br>';
+        echo 'My age is '.$age;
+    }
+    info('Forhad Hoosain Riyad', 27);
+    echo '</br>';
+    info('Salman Farsi', 7); */
+
+    function add($a, $b){
+        return $a+$b;
+    }
+    echo add(2,3);
+
+?>
