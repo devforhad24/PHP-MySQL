c4bda25005d4541765ee11f2506dbd9882e3cbf1
myDesktop <developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.com>
2025-01-29 18:47:47 +0600

    Operator

---
+++ operator.php
@@ -0,0 +1,40 @@
+<?php
+    /**
+     * Assignment operator
+     */
+    //$a = 10;
+    // echo $a /= 2;
+    // echo $a += 2;
+    // echo $a -= 2;
+    // echo $a *= 2;
+
+    /**
+     * Comparison operator
+     */
+    /*     $a = 10;
+    if($a == 10){
+        echo "Ok";
+    } */
+
+    /**
+     * Arithmetic operator
+     */
+    /*     $a = 10;
+    $b = 5;
+    $c = $a+$b;
+    echo $c; */
+
+    /**
+     * Logical operator
+     */
+    $a = 5;
+    $b = 3;
+
+    /* if($a == 5 AND $b == 3){
+        echo 'Done.';
+    } */
+    if($a == 5 OR $b == 2){
+        echo 'Done.';
+    }
+
+?>
