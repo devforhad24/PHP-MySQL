5b314d516a5d03a13404400ee502641346cb59d9
myDesktop <developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.com>
2025-01-29 19:01:37 +0600

    Loop

---
+++ loop.php
@@ -0,0 +1,24 @@
+<?php 
+    // While Loop
+    /* $x = 1;
+    while($x <= 5){
+        echo $x .'</br>';
+        $x++;
+    } */
+
+    // do while loop
+    /* $x = 1;
+    do{
+        echo "All Iz Well $x</br>";
+        $x++;
+    }while($x<6); */
+
+    // for loop
+    $x = 1;
+
+    for($x; $x<7; $x++){
+        echo 'Hello ' .$x.'</br>';
+    }
+
+
+?>
