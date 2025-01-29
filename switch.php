5b81d152443633f49a309b0573dba8bf9de5c8c2
myDesktop <developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.com>
2025-01-29 18:27:54 +0600

    Switch Case statement

---
+++ switch.php
@@ -0,0 +1,17 @@
+<?php
+    $marks = 90;
+
+    switch($marks){
+        case 90:
+            echo 'A+'; break;
+        case 80:
+            echo 'A'; break;
+        case 70:
+            echo 'A-'; break;
+        case 60: 
+            echo 'B'; break;
+        default: 
+            echo 'Fail';
+    }
+
+?>
