4ccb721403cecb7077804126c3a387e854a671a9
myDesktop <developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.com>
2025-01-29 18:23:12 +0600

    if,else if and else statement

---
+++ second.php
@@ -0,0 +1,19 @@
+<?php
+
+/*     $a = 3;
+    if($a ==2 ){
+        echo 'hello';
+    }else{
+        echo 'bye';
+    } */
+
+    $a = '10am';
+    if($a=='10am'){
+        echo 'Good Morning';
+    }else if($a == '12pm'){
+        echo 'Good Afternoon';
+    }else{
+        echo 'Thank You!..';
+    }
+
+?>
