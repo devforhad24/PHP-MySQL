1afec523b965a2d0174fa1f239867f314a620c32
myDesktop <developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.com>
2025-01-30 14:26:22 +0600

    Restricting File Size and Type

--- file/upload.php
+++ file/upload.php
@@ -1,17 +1,34 @@
 <?php
     if(isset($_POST['submit'])){
         $filename = $_FILES['filename']['name'];
+        $filetype = $_FILES['filename']['type'];
+        $filesize = $_FILES['filename']['size'];
+
         $tmp_loc = $_FILES['filename']['tmp_name'];
+
+        $dotpos = strpos($filename,'.')+1;
+        $ext = substr($filename, $dotpos);
         
         $uploc = 'images/';
 
         if(!empty($filename)){
-            move_uploaded_file($tmp_loc, $uploc.$filename);
-            echo 'File uploaded successfully!';
+            if($ext == 'jpg'){
+                if($filetype == 'image/jpeg'){
+                    if($filesize < 90000){
+                        move_uploaded_file($tmp_loc, $uploc.$filename);
+                        echo 'uploaded successfully!';
+                    }else{
+                        echo 'File size less than 90000 bytes';
+                    }
+                }else{
+                    echo 'Select a image';
+                }
+            }else{
+                echo 'Select a jpg image file';
+            }
         }else{
-            echo 'Select a file';
+            echo 'Select a File';
         }
-
     }
 ?>
 
