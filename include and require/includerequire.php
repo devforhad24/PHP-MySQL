cdcc22fb5896c5528d4524b6c3accf98ae843252
myDesktop <developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.com>
2025-01-30 10:46:05 +0600

    Include and Require

---
+++ include and require/includerequire.php
@@ -0,0 +1,13 @@
+<?php
+    // include 'header.inc.php';
+    // require 'header1.inc.php';
+
+    // require_once 'header.inc.php';
+    // require 'header.inc.php';
+
+    include_once 'header.inc.php';
+    include 'header.inc.php';
+
+?>
+
+<h2>Some text here..</h2>
