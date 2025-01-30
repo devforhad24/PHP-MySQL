d0c490b5dbe3a21f238715110a482fee5b69e694
myDesktop <developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.comgit config --global user.email developerforhad24@gmail.com>
2025-01-30 13:40:29 +0600

    File Upload

---
+++ file/upload.php
@@ -0,0 +1,22 @@
+<?php
+    if(isset($_POST['submit'])){
+        $filename = $_FILES['filename']['name'];
+        $tmp_loc = $_FILES['filename']['tmp_name'];
+        
+        $uploc = 'images/';
+
+        if(!empty($filename)){
+            move_uploaded_file($tmp_loc, $uploc.$filename);
+            echo 'File uploaded successfully!';
+        }else{
+            echo 'Select a file';
+        }
+
+    }
+?>
+
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+    Select Image
+    <input type="file" name="filename"> <br><br>
+    <input type="submit" value="Upload" name="submit">
+</form>
